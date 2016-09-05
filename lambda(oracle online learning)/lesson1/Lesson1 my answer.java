5fb103aacffd679219d81f1ea06be38abfd019b9
mameko <tianmeidouzi@gmail.com>
2016-09-05 15:05:02 -0300

    Add files via upload

---
+++ lambda(oracle online learning)/lesson1/Lesson1 my answer.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright © 2014, Oracle and/or its affiliates. All rights reserved.
+ *
+ * JDK 8 MOOC Lesson 1 homework
+ */
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+/**
+ * @author Speakjava (simon.ritter@oracle.com)
+ */
+public class Lesson1 {
+	/**
+	 * Run the exercises to ensure we got the right answers
+	 */
+	public void runExercises() {
+		System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 1");
+		System.out.println("Running exercise 1 solution...");
+		exercise1();
+		System.out.println("Running exercise 2 solution...");
+		exercise2();
+		System.out.println("Running exercise 3 solution...");
+		exercise3();
+		System.out.println("Running exercise 4 solution...");
+		exercise4();
+		System.out.println("Running exercise 5 solution...");
+		exercise5();
+	}
+
+	/**
+	 * All exercises should be completed using Lambda expressions and the new
+	 * methods added to JDK 8 where appropriate. There is no need to use an
+	 * explicit loop in any of the code. Use method references rather than full
+	 * lambda expressions wherever possible.
+	 */
+	/**
+	 * Exercise 1
+	 *
+	 * Create a string that consists of the first letter of each word in the
+	 * list of Strings provided.
+	 */
+	private void exercise1() {
+		List<String> list = Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+
+		/* YOUR CODE HERE */
+		Function<String, Character> pickFirstChar = s -> s.charAt(0);
+		StringBuilder sb = new StringBuilder();
+		for (String s : list) {
+			sb.append(pickFirstChar.apply(s));
+		}
+		System.out.println(sb.toString());
+	}
+
+	/**
+	 * Exercise 2
+	 *
+	 * Remove the words that have odd lengths from the list.
+	 */
+	private void exercise2() {
+		List<String> list = new ArrayList<>(Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+
+		/* YOUR CODE HERE */
+		Predicate<String> testLength = s -> s.length() % 2 != 0;
+		list.removeIf(testLength);
+		System.out.println(list);
+	}
+
+	/**
+	 * Exercise 3
+	 *
+	 * Replace every word in the list with its upper case equivalent.
+	 */
+	private void exercise3() {
+		List<String> list = new ArrayList<>(Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+
+		/* YOUR CODE HERE */
+		UnaryOperator<String> toUpperCase = s -> s.toUpperCase();
+		list.replaceAll(toUpperCase);
+		System.out.println(list);
+	}
+
+	/**
+	 * Exercise 4
+	 *
+	 * Convert every key-value pair of the map into a string and append them all
+	 * into a single string, in iteration order.
+	 */
+	private void exercise4() {
+		Map<String, Integer> map = new TreeMap<>();
+		map.put("c", 3);
+		map.put("b", 2);
+		map.put("a", 1);
+
+		/* YOUR CODE HERE */
+		StringBuilder sb = new StringBuilder();
+		BiConsumer<String, Integer> changeToString = (k, v) -> sb.append(k + v.toString());
+		map.forEach(changeToString);
+		System.out.println(sb.toString());
+	}
+
+	/**
+	 * Exercise 5
+	 *
+	 * Create a new thread that prints the numbers from the list.
+	 */
+	private void exercise5() {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+		/* YOUR CODE HERE */
+		Runnable r = () -> System.out.println(list);
+		new Thread(r).start();
+	}
+
+	/**
+	 * Main entry point for application
+	 *
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		Lesson1 lesson = new Lesson1();
+		lesson.runExercises();
+	}
+}
